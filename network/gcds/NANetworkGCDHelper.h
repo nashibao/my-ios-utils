ff6459c6bd3d50ec38d31bdf2c9054a4b5d9c709
nashibao <nashibao@gmail.com>
2012-10-01 12:43:06 +0900

    add text to NANetworkGCDHelper.

--- network/gcds/NANetworkGCDHelper.h
+++ network/gcds/NANetworkGCDHelper.h
@@ -10,14 +10,33 @@
 
 @interface NANetworkGCDHelper : NSObject
 
-//for managing statusbar indicator
+/*
+ for managing statusbar indicator
+ */
 + (void)networkStart;
 + (void)networkEnd;
 
+#pragma mark TODO: use custom domains
+/*
+ domains
+ */
 + (void)setDomain:(NSString *)domain;
-
 + (NSString *)getDomain;
 
+
+/*
+ main function of this class.
+ 
+ <args>
+ endpoint: url should be domains + endpoint
+ data: request data
+ isPost: boolean for post/get
+ encoding: escape encoding
+ jsonOption: if this param is not nil, callback param(id data) will be a json data.
+ returnMain: it this param is YES, callback func is enclosed by main queue dispatch
+ successHandler/errorHandler: blocks for callback
+ 
+ */
 + (void)sendAsynchronousRequestByEndPoint:(NSString *)endpoint data:(NSDictionary *)data isPost:(BOOL)isPost encoding:(NSStringEncoding)encoding jsonOption:(NSJSONReadingOptions)jsonOption returnMain:(BOOL)returnMain successHandler:(void(^)(NSURLResponse *resp, id data))successHandler errorHandler:(void(^)(NSURLResponse *resp, NSError *err))errorHandler;
 
 @end

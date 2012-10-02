1bd3f76663f5d6ab32cd1fcc6e19976b2e0ac42c
nashibao <nashibao@gmail.com>
2012-10-02 12:11:25 +0900

    add NSDictionary transformable class.

---
+++ coredata/models/NSDictionaryTransformer.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionaryTransformer.h
+//  SK3
+//
+//  Created by nashibao on 2012/10/02.
+//  Copyright (c) 2012年 s-cubism. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionaryTransformer : NSValueTransformer
+
+@end

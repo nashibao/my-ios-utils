545f55a4cdc66bd41bedd8fd30bab12a1f236650
nashibao <nashibao@gmail.com>
2012-11-07 12:23:05 +0900

    macro作成

--- macro/AccessorMacros.h
+++ macro/AccessorMacros.h
@@ -8,6 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
-@interface AccessorMacros : NSObject
-
-@end
+#define INTEGER_ACCESSOR(accessorName, bigAccessorName, keyName, typeName) \
+- (void)bigAccessorName:(typeName)b { \
+    [self willChangeValueForKey:keyName]; \
+    [self setPrimitiveValue:[NSNumber numberWithInt:b] forKey:keyName]; \
+    [self didChangeValueForKey:keyName]; \
+} \
+- (typeName)accessorName{ \
+    [self willAccessValueForKey:keyName]; \
+    NSInteger b = [[self primitiveValueForKey:keyName] intValue]; \
+    [self didAccessValueForKey:keyName]; \
+    return b; \
+} \

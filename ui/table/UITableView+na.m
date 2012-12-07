a37ac634a7c0e1f1335dded2a02f746bd84a3dc0
nashibao <nashibao@gmail.com>
2012-12-07 11:56:48 +0900

    scrollToBottomを追加．

---
+++ ui/table/UITableView+na.m
@@ -0,0 +1,20 @@
+//
+//  UITableView+na.m
+//  SK3
+//
+//  Created by nashibao on 2012/11/30.
+//  Copyright (c) 2012年 s-cubism. All rights reserved.
+//
+
+#import "UITableView+na.h"
+
+@implementation UITableView (na)
+
+- (void)scrollToBottomWithAnimated:(BOOL)animated{
+    NSInteger sectioncnt = [self numberOfSections] -1;
+    if(sectioncnt > -1){
+        [self scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:[self numberOfRowsInSection:sectioncnt]-1 inSection:sectioncnt] atScrollPosition:UITableViewScrollPositionTop animated:animated];
+    }
+}
+
+@end
